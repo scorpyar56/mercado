9bed5bfc0993dd08e85381b19a3d8a8257c2903d
Nicolai Stanschi <shtansky@gmail.com>
2020-01-14 16:08:22 +0200

    probably notifications (#105)

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -118,8 +118,9 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $middlewarePriority = [
-        \Illuminate\Session\Middleware\StartSession::class,
+
         \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+        \Illuminate\Session\Middleware\StartSession::class,
         \App\Http\Middleware\Authenticate::class,
         \Illuminate\Session\Middleware\AuthenticateSession::class,
         \Illuminate\Routing\Middleware\SubstituteBindings::class,
