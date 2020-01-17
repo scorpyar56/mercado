171ac90cabce01f551cd469ffc8689a72f25d8b7
Nicolai Stanschi <shtansky@gmail.com>
2020-01-17 10:32:46 +0200

    automaticdeploy (#139)
    * Merge branch 'master' of E:\Projects\999\Lara with conflicts.
    
    * trying to deploy automatically
    
    * trying to deploy automatically
    
    * trying to deploy automatically
    
    * trying to deploy automatically
    
    * trying to deploy automatically
    
    * trying to deploy automatically
    
    * deploy automatically

--- public/deploy.php
+++ public/deploy.php
@@ -6,11 +6,12 @@ if ($payload['action']=='closed') {
     echo exec('whoami') . PHP_EOL;
     echo exec('cd /var/www/lara && git stash && git pull origin master') . PHP_EOL;
     $f = fopen("autoupdate.log", 'a+');
+    fwrite($f, "---------------" . PHP_EOL);
     fwrite($f, date('c') . PHP_EOL);
     fwrite($f, "Sender: ". $payload['sender']['login']. PHP_EOL);
     fwrite($f, "Pull request URL: ". $payload['pull_request']['url']. PHP_EOL);
     fwrite($f, "Pull request Title: ". $payload['pull_request']['title']. PHP_EOL);
-    fwrite($f, "Pull request user: ". $payload['pull_request']['user']['login']. PHP_EOL);
+    fwrite($f, "Pull request user: ". $payload['pull_request']['user']['login']. PHP_EOL. PHP_EOL);
 }
 
 fclose($f);
